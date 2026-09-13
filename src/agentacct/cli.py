ed7d130666032dbbb542b5f5a739ea6da627f04d
chayu <3317038611@qq.com>
2026-09-13 13:43:04 +0800

    cli: add -V short alias for --version

--- src/agentacct/cli.py
+++ src/agentacct/cli.py
@@ -209,6 +209,7 @@ def _app_main(
         Optional[bool],
         typer.Option(
             "--version",
+            "-V",
             callback=_version_callback,
             is_eager=True,
             help="Show the installed agentacct version and exit.",
